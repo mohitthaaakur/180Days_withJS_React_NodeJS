ed8e6352a944546ce169fb15a438fb29332799c9
mohitthaaakur <mohitthakur.2583@gmail.com>
2026-07-27 23:33:19 +0530

    Refactor and enhance array flattening functions
    Refactor deep flattening functions and add new functionality for single nested arrays.

--- Arrays/Day-15/15.js
+++ Arrays/Day-15/15.js
@@ -1,17 +1,36 @@
-let breadCrumbs = ['home', 'products', 'electronics', 'laptops', 'gaming'];
-let pathStr = breadCrumbs.reduceRight((preVal, curVal) => {
- return preVal += ' > ' + curVal
-});
-console.log(pathStr);
-let nestedArr = [1, [2, [3, [4, [5]]]]];
-let nestedObj = { id: 1, id2: { a: 'sadasd'}, s: 'dfds',}
-function deepFlatten (array) {
- let deepFlattenArr = [];
- if ( !(Array.isArray(array))) return array;
-  array.forEach(element => {
-   if ( (Array.isArray(element))) {
-     
-   }
-  }); 
-};
-console.log(deepFlatten(nestedArr));
+       let breadCrumbs = ['home', 'products', 'electronics', 'laptops', 'gaming'];
+       let pathStr = breadCrumbs.reduceRight((preVal, curVal) => {
+        return preVal += ' > ' + curVal
+       });
+       console.log(pathStr);
+       let nestedArr = [1, [2, [3, [4, [5]]]]];
+       let nestedObj = { id: 1, id2: { a: 'sadasd'}, s: 'dfds',}
+       function singleNestArr (arr){
+        let values = [];
+        for (let key in arr) {
+          for (let items of arr[key]) {
+            values.push(items);
+          }
+        }
+        return values;
+       };
+       function deepNest (arr){
+        let flatArr = [];
+        if ( !(Array.isArray(arr))) return arr;
+        for (let items of arr) {
+          if (!(Array.isArray(items))){
+            flatArr.push(items);
+          } else if(Array.isArray(items)){
+            let val = deepNest(items);
+            for (let item of val) {
+              flatArr.push(item);
+            }            
+         }        
+       }
+       return flatArr;
+      };
+      let str = 'heikcisc'
+      let nstArr = [1, 'two', [3, [4]], true];
+      console.log(deepNest(nestedArr));
+      console.log(deepNest(str));
+      console.log(deepNest(nstArr));
