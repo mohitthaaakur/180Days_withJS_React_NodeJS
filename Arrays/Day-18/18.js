873d5aecb2edf7f2cc4916de962a04df0fee1751
mohitthaaakur <mohitthakur.2583@gmail.com>
2026-07-29 23:59:37 +0530

    Add files via upload

---
+++ Arrays/Day-18/18.js
@@ -0,0 +1,12 @@
+      // Sort in decreasing order 
+      let arr = [5,2,1,-10,8];
+      arr.sort((a, b) => b - a);
+      console.log(arr);
+      // Copy and sort array
+      let arr2 = ['HTML', 'JavaScript', 'CSS'];
+      function copySorted(arr){
+
+      };
+      let sorted = copySorted(arr2);
+      console.log(sorted);
+       
