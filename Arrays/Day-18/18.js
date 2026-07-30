f64c3604c5c53bf4f667bf8d766ed9cd8639a4b0
mohitthaaakur <mohitthakur.2583@gmail.com>
2026-07-30 23:29:17 +0530

    Add copySorted function for sorting array
    Implemented copySorted function to return a sorted copy of the input array.

--- Arrays/Day-18/18.js
+++ Arrays/Day-18/18.js
@@ -5,8 +5,13 @@
       // Copy and sort array
       let arr2 = ['HTML', 'JavaScript', 'CSS'];
       function copySorted(arr){
-
+        // let sortedArr = [];
+        // arr.forEach(element => {
+        //   sortedArr.push(element)
+        // });
+        // return sortedArr.sort((a,b) => a.localeCompare(b)); 
+        return arr.slice().sort((a,b) => a.localeCompare(b));
       };
       let sorted = copySorted(arr2);
       console.log(sorted);
-       
+      console.log(arr2);
