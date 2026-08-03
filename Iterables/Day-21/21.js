f65bb595f5bad663a348e3976e65d3f4802c059b
mohitthaaakur <mohitthakur.2583@gmail.com>
2026-08-03 23:58:05 +0530

    Update 21.js

--- Iterables/Day-21/21.js
+++ Iterables/Day-21/21.js
@@ -5,17 +5,34 @@ let range = {
 range[Symbol.iterator] = function(){
   return {
     current: this.from,
-    last: this.to,
-  
-  next(){
-    if (this.current <= this.list) {
-      return { done: false, value: this.current++ };
+    final: this.to,
+    next(){
+      if ( this.current <= this.final){
+       return {done: false, value: this.current++ };
+      } else {
+       return { done: true};
+      }
+    }
+  }
+};
+for (let iterator of range) {
+  console.log(iterator);
+}; 
+let range2 = {
+  from: 5,
+  to: 1,
+  [Symbol.iterator] (){
+    this.current = this.from;
+    return this
+  },
+  next() {
+    if (this.current >= this.to){
+      return { done: false, value: this.current-- };
     } else {
       return { done: true };
     }
   }
-}
 };
-for (let num of range) {
-  console.log(num);
+for (let iterator of range2) {
+  console.log(iterator)
 };
