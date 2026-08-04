f2cf29482309c4dabf445779f7f7c2a33845fd5d
mohitthaaakur <mohitthakur.2583@gmail.com>
2026-08-04 23:48:56 +0530

    Add files via upload

---
+++ Iterables/Day-24/24.js
@@ -0,0 +1,15 @@
+// Chunk iterator
+let arrLike = {
+  0: 'a',
+  1: 'b',
+  2: 'c',
+  3: 'd',
+  length: 4,
+};
+let arr = Array.from(arrLike, (item, index) => `${item}-${index}`);
+console.log(arr);
+// Merge two iterables
+function zip(iterable1, iterable2){
+
+};
+// Infinite iterator + take
