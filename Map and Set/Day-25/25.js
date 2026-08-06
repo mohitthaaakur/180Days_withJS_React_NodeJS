4a0d401054fc4fdff2644f9daff11d9d393d9e6b
mohitthaaakur <mohitthakur.2583@gmail.com>
2026-08-06 23:43:03 +0530

    Enhance Map usage examples and functionality
    Added various operations and examples for Map, including handling NaN, iteration, and converting between objects and maps.

--- Map and Set/Day-25/25.js
+++ Map and Set/Day-25/25.js
@@ -1,11 +1,43 @@
 let map = new Map();
-map.set('1', 'str1');
-map.set(1, 'num1');
-map.set(true, 'bool1');
-console.log(map.get(1));
-console.log(map.get('1'))
-console.log(map.size);
-let john = { name: 'john'};
-let visitsCountMap = new Map();
-visitsCountMap.set(john, 123);
-console.log( visitsCountMap.get(john));
+      map.set(1, 'num1');
+      map.set('1', 'str1');
+      map.set(true, 'bool1');
+      console.log(map.get(1));
+      console.log(map.get(true));
+      console.log(map.size);
+      map.set(NaN, 'nan1') // only in map NaN === NaN : true
+      console.log(map.get(NaN));
+      map.set(2, 'num2').set(3, 'num3').set(false, 'falseval');
+      console.log(map.size); // 7
+      // iteration over map
+      let vegetables = new Map([
+        [ 'tomato', 200],
+        [ 'cucumber', 345],
+        [ 'onion', 534],
+      ])
+      for ( let key of vegetables.keys()){
+        console.log(key);
+      };
+      for ( let values of vegetables.values()){
+        console.log(values);
+      };
+      for ( let entries of vegetables){ // same as vegetables.entries()
+        console.log(entries);
+      };
+      vegetables.forEach((values, key, map) => {
+        console.log(`${key} is sold at $${values}`)
+      });
+      // object to map
+      let users = {
+        1: 'john',
+        user2: 'anne',
+      };
+      let userMap = new Map(Object.entries(users));
+      console.log(userMap.get('user2'));
+      // object from map
+      let map2 = new Map();
+      map2.set('green', 4);
+      map2.set(3, 'red');
+      map2.set(users, 2);
+      let obj = Object.fromEntries(map2);
+      console.log(obj[users]) // 2
